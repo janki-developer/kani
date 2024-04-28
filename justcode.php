6eb5dc05d6c5131386c85b0967fa898e37c7ed5b
janki-developer <168222489+janki-developer@users.noreply.github.com>
2024-04-28 10:14:28 +0530

    Update justcode.php

--- justcode.php
+++ justcode.php
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-<h3>hellow duniyaaa</h3>;
-<h3>hellow duniyaaa</h3>;
+
 <?php 
 
 $a=20;
@@ -10,6 +8,3 @@ $c=$a+$b;
 echo "Sum:",$c;
 
 ?>
-=======
-<h3>hellow duniyaaa</h3>
->>>>>>> 203ce3c02aed50393962a726871953b1550b0897

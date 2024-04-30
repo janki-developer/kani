d5006e08a4deac6725399619e91370c539ee0940
janki-developer <168222489+janki-developer@users.noreply.github.com>
2024-04-30 19:30:14 +0530

    Update justcode.php

--- justcode.php
+++ justcode.php
@@ -1,6 +1,9 @@
+<!--php keywords are not case sensative like(if,else,while,echo),classes,function,user-difined function are not cs-->
+<!--php variable are start with character and underscore(not by number)(it contains alpha numaric character and _(A-z,0-9,and-))-->
+<!--php variable are case sensative in php-->
 
+<!--variable declaration in php-->
 <?php 
-
 $a=20;
 $b=30;
 
@@ -8,6 +11,21 @@ $c=$a+$b;
 echo "Sum:",$c;
 ?><br>
 
+<!--sum of two number in php-->
+<?php 
+$a=200;
+$b=300;
+echo "sum of two nimber is:",$a+$b;
+?><br>
+
+<!--php support data type string,integer,float(double),boolean,array,object,null,resource(we can assign a one value to multiple variable)-->
+<!--to get the data type we can use var_dump--><br>
+<?php 
+$a= null;
+var_dump($a);
+?><br> 
+
+<!--switch case in php-->
 <?PHP 
 $color="red";
 $color="green";
@@ -21,7 +39,7 @@ case "green":
     break;
 }
 ?><br>
-
+<!--switch with defoult value in php-->
 <?php
 switch($color){
     case "red":
@@ -35,7 +53,7 @@ case "yellow":
     echo "neighter";
 }
 ?><br>
-<!--while loop-->
+<!--while loop. it at stating first check the condition if its true then it enters the loop and executes the block of code(entry control loop)-->
 <?php 
 $i=1;
 while($i <6){

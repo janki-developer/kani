fdd877f9667cbd387789b2c40e536fc4a207f937
janki-developer <168222489+janki-developer@users.noreply.github.com>
2024-05-11 23:48:45 +0530

    javascripp1

--- justcode.php
+++ justcode.php
@@ -1,9 +1,15 @@
 <!--php keywords are not case sensative like(if,else,while,echo),classes,function,user-difined function are not cs-->
 <!--php variable are start with character and underscore(not by number)(it contains alpha numaric character and _(A-z,0-9,and-))-->
+<!--all variable name are case sencative-->
 <!--php variable are case sensative in php-->
+<!--php 8.2.12 is latest version of php(php 7 supports new operator <=>)-->
+<!-- php statement end with ;-->
+<!--php variable scope 1.local 2.global 3.static-->
+
 
 <!--variable declaration in php-->
 <?php 
+
 $a=20;
 $b=30;
 
@@ -22,43 +28,73 @@ echo "sum of two nimber is:",$a+$b;
 <!--to get the data type we can use var_dump--><br>
 <?php 
 $a= null;
-var_dump($a);
+var_dump($a); //is used to find the datatype of php
 ?><br> 
 
-<!--switch case in php-->
+<!--switch case with function in php-->
 <?PHP 
-$color="red";
 $color="green";
 
 switch($color){
     case "red":
 echo "hello";
+
 break;
 case "green":
     echo "welcome";
     break;
 }
-?><br>
+?>
+<br>
+
 <!--switch with defoult value in php-->
-<?php
+<?php 
+$color="red";
+
 switch($color){
     case "red":
-echo "hello";
-break;
-case "yellow":
-    echo "welcome";
-    break;
+        echo "hello";
+        break;
+        case "green":
+            echo "welcome";
+            break;
+            default:
+            echo "neigther";
 
-    default:
-    echo "neighter";
 }
 ?><br>
 <!--while loop. it at stating first check the condition if its true then it enters the loop and executes the block of code(entry control loop)-->
+<!--four type of loop in php for,while , do while, foreach-->
 <?php 
 $i=1;
 while($i <6){
-echo "$i";
+echo "$i <br>";
 $i++;
+}
+?><br>
+
+<?php // do while loop
+$i=1;
+do{
+    echo $i;
+    $i++;
+}
+    while($i < 10)
+
+?><br>
+
+<!--for loop-->
+<?php 
+for($i=0; $i<20; $i++){
+    echo "$i";
+}
+?><br>
+
+<!-- foreach loop-->
+<?php 
+$colors = array("red" , "green" , "blue" , "yellow");
+foreach($colors as $x){
+    echo  "$x <br>";
 }
 ?><br>
 
@@ -86,4 +122,69 @@ while($i <100){
     echo "$i<br>";
     $i+=10;
 }
+?><br>
+
+
+<?php 
+echo phpversion();// it desdribe the latest version of php
+?><br>
+
+<!--php string function-->
+<?php 
+$a="janki";
+$b="kami";
+echo strlen ($a);
+//echo strlen('hello php'); another way to find strlength
+?><br>
+
+<?php 
+echo strrev('hello php string'); // for reverse string
+?><br>
+
+<?php 
+$oldtxt = "hello janki";
+echo str_replace("janki", "kami", $oldtxt);
+?><br>
+
+<?php //php multply we can add like00(+,-,/)
+echo 100*200
+?><br>
+
+<?php //php conditional ststement if, elseif, ifelse, if elseif else, switch
+$m=40;
+$n=10;
+if ($m > $n) { //
+  echo "hello php";
+}
+?><br>
+
+<?php // if two condition not same then print hello php world
+$m=40;
+$n=10;
+if ($m != $n) { //
+  echo "hello php world";
+}
+?><br>
+
+<?php // if condition same then print yes otherwise no
+$m=40;
+$n=10;
+if ($m == $n) { //
+  echo "yes";
+} else{
+    echo "no";
+}
+?><br>
+
+<?php // if t is equal s then print 1, t>s then print 2 otherwise 3
+$t=50;
+$s=50;
+if($t==$s){
+    echo "1";
+}elseif($t > $s){
+    echo "2";
+}else{
+     echo "3";
+}
 ?>
+
